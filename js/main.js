18b7670f2ca3a45a6f682fe95309ffa99f6708aa
ving9 <rhymester139@gmail.com>
2020-03-10 12:42:49 +0900

    20.03.10 처음 업로드

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// function inputEventInit(){
+//     ex.addEventListener('mousewheel', function(delta){
+//         if(delta.wheelDelta >= 0){
+//             console.log('+');
+//         }else{
+//             console.log('-');
+//         }
+//     });
+
+//     ex.addEventListener('mousedown', function(event){
+//         console.log('mousedown');
+//     });
+
+//     ex.addEventListener('mouseup', function(event){
+//         console.log('mouseup');
+//     })
+// };
+
+// if(e.wheelDelta === -120){
+//     console.log('wheel down');
+//     } else {
+//     console.log('wheel up');
+//     }
+
+
