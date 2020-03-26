419453715a222438bff702a28c9f647c44cddde7
ving9 <rhymester139@gmail.com>
2020-03-26 17:25:41 +0900

    20.03.26 워크페이지 디자인/비디오 추가

--- js/works.js
+++ js/works.js
@@ -7,15 +7,35 @@ $(function(){
             breakpoint: 768,
             settings: {
                 arrows: false,
+                slidesToShow: 1,
                 }
-            },
-            {
-            breakpoint: 480,
+            }],
+        
+    });
+    $('.s2_video').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        responsive: [{
+            breakpoint: 768,
             settings: {
                 arrows: false,
+                slidesToShow: 1,
                 }
-            }
-        ]
+            }],
+        
+    });
+
+    $('.s2_design .slick-slide').click(function(){
+        $('body').css("overflow","hidden");
+        let num = $(this).attr('data-slick-index');
+        $('.design_cover li').hide();
+        $('.design_cover li').eq(num).css("display","flex");
+        console.log(num);
+    });
+    $('.design_cover li').click(function(){
+        $('body').css("overflow","visible");
+        $('.design_cover li').hide();
     });
 });
     
