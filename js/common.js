239790213edfb2f4e3147d4a1921da017b7f2100
ving9 <rhymester139@gmail.com>
2020-03-30 17:25:57 +0900

    20.03.30 워크페이지 완료 어바웃페이지 시작

--- js/common.js
+++ js/common.js
@@ -2,7 +2,8 @@ const
     btn_menu = document.querySelector('.btn_menu'),
     modal_wrap = document.querySelector('.modal_wrap'),
     section4 = document.getElementById('section4'),
-    menu_contact = document.querySelector('.menu_contact');
+    menu_contact = document.querySelector('.menu_contact'),
+    btn_pageup = document.querySelector('.btn_pageup');
 
 
 btn_menu.onclick = function() {
@@ -26,3 +27,19 @@ menu_contact.onclick = function() {
     window.scrollTo(0, section4.offsetTop);
     modal_close();
 };
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > document.body.offsetHeight*0.7) {
+        btn_pageup.style.opacity=1
+    }else {
+        btn_pageup.style.opacity=0
+    }
+});
+
+btn_pageup.onclick = function() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
